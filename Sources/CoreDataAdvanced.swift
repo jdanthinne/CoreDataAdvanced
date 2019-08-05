d84bd54284e43be148d3c8d5590f4d9bd94b1e17
Jérôme Danthinne <jerome@grincheux.be>
2019-08-05 21:39:52 +0200

    SwiftLint

--- Sources/CoreDataAdvanced.swift
+++ Sources/CoreDataAdvanced.swift
@@ -7,8 +7,8 @@
 //
 
 // Include Foundation
-@_exported import Foundation
 @_exported import CoreData
+@_exported import Foundation
 import UIKit
 
 open class ManagedObject: NSManagedObject {}
@@ -19,8 +19,8 @@ public protocol ManagedObjectContextSettable: AnyObject {
 
 public extension ManagedObjectContextSettable {
     func injectManagedObjectContext(in segue: UIStoryboardSegue) {
-        if let vc = segue.destination as? ManagedObjectContextSettable {
-            vc.managedObjectContext = managedObjectContext
+        if let viewController = segue.destination as? ManagedObjectContextSettable {
+            viewController.managedObjectContext = managedObjectContext
         }
     }
 }
@@ -30,33 +30,39 @@ public extension NSManagedObjectContext {
                                   filename: String,
                                   in directory: FileManager.SearchPathDirectory = .documentDirectory)
         -> NSManagedObjectContext {
-        let bundles = models.map { Bundle(for: $0) }
-        
-        guard let model = NSManagedObjectModel.mergedModel(from: bundles)
-            else { fatalError("model not found") }
-        
-        let storeURL = FileManager.default
-            .urls(for: directory, in: .userDomainMask)
-            .first?
-            .appendingPathComponent(filename)
-        
-        let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
-        try! psc.addPersistentStore(ofType: NSSQLiteStoreType,
-                                    configurationName: nil,
-                                    at: storeURL,
-                                    options: nil)
-        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-        context.persistentStoreCoordinator = psc
-        
-        return context
+            let bundles = models.map { Bundle(for: $0) }
+            
+            guard let model = NSManagedObjectModel.mergedModel(from: bundles)
+                else { fatalError("model not found") }
+            
+            let storeURL = FileManager.default
+                .urls(for: directory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(filename)
+            
+            let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
+            
+            do {
+                try psc.addPersistentStore(ofType: NSSQLiteStoreType,
+                                           configurationName: nil,
+                                           at: storeURL,
+                                           options: nil)
+                let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+                context.persistentStoreCoordinator = psc
+                
+                return context
+            } catch {
+                fatalError("Unable to add Persistent Store: \(error)")
+            }
     }
     
     func inject(in viewController: UIViewController?) {
-        guard let viewController = viewController else { return }
+        guard let viewController = viewController
+            else { return }
         
-        guard let vc = viewController as? ManagedObjectContextSettable
+        guard let viewControllerToInject = viewController as? ManagedObjectContextSettable
             else { fatalError("Unable to set \(viewController) as ManagedObjectContextSettable") }
         
-        vc.managedObjectContext = self
+        viewControllerToInject.managedObjectContext = self
     }
 }
