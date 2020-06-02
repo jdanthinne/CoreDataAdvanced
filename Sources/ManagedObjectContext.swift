a42355dd642d134a3c26fcf9538dd796598d4e53
Jérôme Danthinne <jerome@grincheux.be>
2020-06-02 18:21:31 +0200

    ManagedObjectContext: saveIfNeeded()

---
+++ Sources/ManagedObjectContext.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jérôme Danthinne on 02/06/2020.
+//
+
+@_exported import CoreData
+
+extension NSManagedObjectContext {
+    /// Only performs a save if there are changes to commit.
+    /// - Returns: `true` if a save was needed. Otherwise, `false`.
+    @discardableResult public func saveIfNeeded() throws -> Bool {
+        guard hasChanges else { return false }
+        try save()
+        return true
+    }
+}
